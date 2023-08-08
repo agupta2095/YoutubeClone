6292393629d6e7207f565f57e6f26467a82d94ad
agupta2095 <guptaakanksha2196@yahoo.in>
2023-08-07 21:29:07 -0700

    Added Youtube logo, add firebase SDK with Auth

---
+++ yt-client-ui/my-app/app/firebase/firebase.ts
@@ -0,0 +1,46 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {
+    getAuth, 
+    signInWithPopup, 
+    GoogleAuthProvider,
+    onAuthStateChanged, 
+    User
+} from "firebase/auth"
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA27DKHBjYIKq_etjnDVC_Y6GiyQRChenQ",
+  authDomain: "yt-clone-akgu.firebaseapp.com",
+  projectId: "yt-clone-akgu",
+  appId: "1:204028752318:web:0fd8f77cfb7661c88609ef"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+/**
+ * Signs the user in with a Google popup
+ * @returns A promise that resolves with the user's credentials
+ */
+export function signInWithGoogle() {
+    return signInWithPopup(auth, new GoogleAuthProvider());
+}
+
+/**
+ * Signs out the user.
+ * @returns A promise that resolves when the user is signed out.
+ */
+export function signOut() {
+   return auth.signOut();
+}
+
+/**
+ * Triggers a callback when user auth state changes
+ * @returns A function to unsubsrcibe callback
+ */
+export function onAuthStateChangedHelper(callback: (user: User | null) => void) {
+    return onAuthStateChanged(auth, callback);
+}
--- yt-client-ui/my-app/app/navbar/navbar.tsx
+++ yt-client-ui/my-app/app/navbar/navbar.tsx
@@ -1,7 +1,13 @@
+import Image from "next/image";
+import styles from "./navbar.module.css";
+import Link from "next/link";
+
 export default function Navbar() {
     return (
-    <div>
-        <h1> NavBar </h1>
-    </div>
+        <nav className={styles.nac}>
+            <Link className={styles.logoContainer} href="/">
+                <Image width={100} height={30} src="/youtube-logo.svg" alt="Youtube Logo" />
+            </Link>
+        </nav>
     );
 }
